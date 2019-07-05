18ae524e668a10e4ea41742517deebe66f64ab07
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-07-04 22:12:43 -0400

    create right word selection in console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,6 +25,9 @@ $(document).ready(() => {
   
 
   //DOM manipulation
+  let domUnderScore = document.getElementsByClassName('underscore');
+  let domRightGuess = document.getElementsByClassName('rightchoice');
+  let domWrongGuess = document.getElementsByClassName('wronguess');
   
   
   console.log(chooseAnimals);
@@ -32,6 +35,8 @@ $(document).ready(() => {
 let generateUnderscore = () => {
     for(let i=0; i <chooseAnimals.length; i++){
         underScore.push('_');
+        
+        
     }
     return underScore;
 }
@@ -44,8 +49,11 @@ document.addEventListener('keypress', (event) => {
     if(chooseAnimals.indexOf(keyWord) > -1){
         //add to right words array
         rightWord.push(keyWord);
+        
         //replace the underscore with right keyword
         underScore[chooseAnimals.indexOf(keyWord)] = keyWord;
+        domUnderScore[0].innerHTML = underScore.join(' ');
+        domRightGuess[0].innerHTML = rightWord;
         //Checks to see if user word matches guesses
         if(underScore.join('') === chooseAnimals){
             alert('You win');
@@ -54,7 +62,7 @@ document.addEventListener('keypress', (event) => {
         console.log(underScore);
     }else {
         wrongWord.push(keyWord);
-
+        domWrongGuess[0].innerHTML = wrongWord;
         console.log(wrongWord);
     }
     
