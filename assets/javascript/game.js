ed7e5864b7a1bad87441747999b1108d58af01fe
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-07-07 13:21:52 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,37 @@
 // Jquery for starting the page
 
-$(document).ready(() => {
-    const $mainButton = $('.main-button');
-    const $navButton = $('.navbutton');
-    const $winLoose = $('.win-loose');
+// $(document).ready(() => {
+//     const $mainButton = $('.main-button');
+//     const $navButton = $('.navbutton');
+//     const $winLoose = $('.win-loose');
 
-    $mainButton.hide();
+//     $mainButton.hide();
     
 
-    $navButton.on('click', () => {
-        $mainButton.show();
-        $navButton.hide();
-        //$winLoose.hide();
-    })
+//     $navButton.on('click', () => {
+//         $mainButton.show();
+//         $navButton.hide();
+//         //$winLoose.hide();  
+//     })
+// })
 
-})
+//const myAnimals = ['Tiger', 'Elephant', 'Dog', 'Horse', 'Zebra','Rhinoceros','Deer', 'Buffalo', 'Ziraf','Lion'];
+const myAnimals = [{name: "Tiger", image:"assets/images/mytiger.jpeg"},
+                    {name: "Elephant", image:"assets/images/mytiger.jpeg"},
+                    {name: "Dog", image:"assets/images/mytiger.jpeg"},
+                    {name: "Horse", image:"assets/images/mytiger.jpeg"},
+                    {name: "Zebra", image:"assets/images/mytiger.jpeg"},
+                    {name: "Rhinoceros", image:"assets/images/mytiger.jpeg"},
+                    {name: "Deer", image:"assets/images/mytiger.jpeg"},
+                    {name: "Buffalo", image:"assets/images/mytiger.jpeg"},
+                    {name: "Ziraf", image:"assets/images/mytiger.jpeg"},
+                    {name: "Lion", image:"assets/images/mytiger.jpeg"}
+                  ]
 
-const myAnimals = ['Tiger', 'Elephant', 'Dog', 'Horse', 'Zebra','Rhinoceros','Deer', 'Buffalo', 'Ziraf','Lion'];
+// let randomNum = Math.floor(Math.random() * myAnimals1.length);
+// console.log(randomNum);
+// console.log(myAnimals1[randomNum].image);
+
 let winCounter = 0;
 let loss = 0;
 let wrongLetter = [];
@@ -32,43 +47,17 @@ let rightLetter = [];
 
 
 function startGame(){
-    randWord = myAnimals[Math.floor(Math.random() * myAnimals.length)];
+    randWord = Math.floor(Math.random() * myAnimals.length);
+    randWord = myAnimals[randWord].name;
     randWord = randWord.toLowerCase();
+    console.log(randWord);
 
     for(let i= 0; i <randWord.length; i++){
         underScore.push('_');
     }
-    console.log(randWord);
     document.getElementById('underscore').textContent = underScore.join(" ");
 
-    
-    //HTML 
-    document.getElementById('countnumber').textContent = guessesLeft;
-    document.getElementById('yourscore').textContent = yourScore;
-    document.getElementById('computerscore').textContent = computerScore;
-
-     
-
-}
-
-
-//wincounter
-
-function winLose(){
-    if(winCounter === randWord.length){
-        document.getElementById('winner').innerHTML = "You win !!!";
-        yourScore++;
-        document.getElementById('yourscore').innerHTML = yourScore;
-
-    }else if(guessesLeft === 0){
-        document.getElementById('looser').innerHTML = "You loose (:";
-        computerScore++;
-        document.getElementById('computerscore').innerHTML = computerScore;
-        
-    }
-}
-    
-//user Guesses
+    //user Guesses
 
     document.onkeypress = function(event){
         userGuesses = event.key.toLowerCase();
@@ -76,29 +65,62 @@ function winLose(){
             for(let i = 0; i <randWord.length; i++){
                 if(randWord[i] === userGuesses){
                     underScore[i] =userGuesses;
-                    console.log(underScore);
+                    //console.log(userGuesses);
                     document.getElementById('underscore').textContent = underScore.join(" ");
                     document.getElementById('rightchoice').textContent = underScore.join(" ");
                     winLose();
-                    winCounter++;
+                    ++winCounter;
                 }
                 
             }
             
         }else{
             wrongLetter.push(userGuesses);
-            if(guessesLeft != 0){
-                guessesLeft--;
+
+            if(guessesLeft >= 0){
+                
             document.getElementById('countnumber').textContent = guessesLeft;
+            guessesLeft--;
             }
             document.getElementById('wrongchoice').textContent = wrongLetter.join(" ");
-            winLose();
-            
+            winLose(); 
         }
     }
 
+    //Reset
     
+
+    //HTML 
+    document.getElementById('countnumber').textContent = guessesLeft;
+    document.getElementById('yourscore').textContent = yourScore;
+    document.getElementById('computerscore').textContent = computerScore;
+
+
+}
+
 startGame();
+
+//wincounter
+
+function winLose(){
+    if(winCounter === randWord.length){
+        document.getElementById('winner').innerHTML = "You win !!!";
+        document.getElementById('myImg').src ="./assets/images/animal.png";
+        yourScore++;
+        document.getElementById('yourscore').innerHTML = yourScore;
+
+    }else if(guessesLeft <= 0){
+        document.getElementById('looser').innerHTML = "You loose (:";
+        ++computerScore;
+        document.getElementById('computerscore').innerHTML = computerScore;
+        
+    }
+}
+    
+
+
+    
+
 
 
 
